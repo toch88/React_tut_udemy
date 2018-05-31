69cf115c65a223f0d233d2859349c51e477f1932
Mateusz <tocha.mateusz@gmail.com>
2018-05-31 11:14:41 +0200

    First component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import './App.css';
-
+import Person from './Person/Preson'
 // import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi</h1>
-    //   </div>
-    // );
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'));
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+        <p>this is working</p>
+        <Person/>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'));
   }
 }
 
---
+++ src/Person/Preson.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const preson:any=()=>{
+    return <p>Function</p>
+};
+
+export default preson
