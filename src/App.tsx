fd61e22fc92d45c56de90c06cacd25b4804ac72a
Mateusz <tocha.mateusz@gmail.com>
2018-05-31 10:58:36 +0200

    Behind the tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
 class App extends React.Component {
   public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
+    // return (
+    //   <div className="App">
+    //     <h1>Hi</h1>
+    //   </div>
+    // );
+    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi'));
   }
 }
 
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-class Startup {
-    public static main(): number {
-
-        console.log("Hello ");
-        return 0;
-    }
-}
-let zmienna = 5;
-Startup.main();
