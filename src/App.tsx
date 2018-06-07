304c2a438db74740e8d4c85322a54799b48e127a
Mateusz <tocha.mateusz@gmail.com>
2018-06-07 20:10:28 +0200

    Bad way of doing binding

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 // import { SyntheticEvent } from 'react';
 import './App.css';
-import Person, { IPersonPropsType } from './Person/Preson';
+// import Person, { IPersonPropsType } from './Person/Preson';
 // import logo from './logo.svg';
 
 class App extends React.Component {
@@ -11,97 +11,28 @@ class App extends React.Component {
       { key: 'aasd', name: 'Max', age: 5 },
       { key: 'dsdf', name: 'Steve', age: 15 },
       { key: 'fhgj', name: 'Matt', age: 25 }
-    ] as IPersonPropsType[],
+    ],
     showPersons: false as boolean
   };
-  public persons:JSX.Element;
-  constructor(props:any){
-    super(props);
 
-
-    
-
-    if (this.state.showPersons) {
-      this.persons = (
-        <div>
-          {this.state.persons.map(
-            (person, index) => {
-              return <Person
-                name={person.name}
-                age={person.age}
-                click={this.deletePersonHandler.bind(this, index)}
-                key={person.key}
-                changed={(event:React.FormEvent<HTMLInputElement>)=>this.nameChangeHandler(event, person.key)}
-                // changed={this.nameChangeHandler.bind(this, person.key)}
-                 />;
-            }
-          )}
-        </div>
-      );
-    }
-
-  }
-  public togglePersonHandler = () => {
-    const doseShow = this.state.showPersons;
-    this.setState({ showPersons: !doseShow });
+  public handleClick(key:number){
+    console.log(`Trybi ${key}`);
   }
 
-  public nameChangeHandler=( e:React.FormEvent<HTMLInputElement>, id:string,):void=>{
-
-    const personIndex = this.state.persons.findIndex(
-      p=>{
-        return p.key===id;
-      }
-    );
-
-    const person ={
-      ...
-      this.state.persons[personIndex]
-    }; 
-    // creating new object with spread operator, alternative Object.assing({},this.state.persons[personIndex])
-
-    person.name =e.currentTarget.value;
-    const persons=[...this.state.persons];
-    persons[personIndex]=person;
-    
-    this.setState({persons});
-    
-  }
-
-  public deletePersonHandler = (personIndex: number): any => {
-    // const persons=this.state.persons.slice();
-    const persons = [...this.state.persons]; // new array with elements form person
-    persons.splice(personIndex, 1);
-    this.setState({ persons }); // ({persons: persons}) it's equal
-  }
   public render() {
-
-    const style: React.CSSProperties = {
-      backgroundColor: 'white',
-      border: '1px solid blue',
-      font: 'inherit',
-      padding: '8px',
-    };
-
-
-
-    // tslint:disable-next-line:jsx-self-close
     
-
     return (
-      <div className="App">
-        <h1>Hi</h1>
-        <p>this is working</p>
-        <button
-          style={style}
-          onClick={this.togglePersonHandler}>
-          Switch Name
-        </button>
-        {this.persons}
-      </div>
+      <ul>
+        {this.state.persons.map(person =>
+          <li key={person.key} onClick={this.handleClick.bind(this, person.key)}>
+            {person.name}
+          </li>
+        )}
+      </ul>
     );
-
+  
   }
+
 }
 
 export default App;
--- src/Person/Preson.tsx
+++ src/Person/Preson.tsx
@@ -1,36 +1,33 @@
-import * as React from 'react';
-import './Person.css';
+// import * as React from 'react';
+// import './Person.css';
 
 
-export interface IPersonPropsType {
-    key:AAGUID;
-    name: string;
-    age: number;
-    click?: any;
-
-    changed?: (e:React.FormEvent<HTMLInputElement>)=>void;
-}
+// export interface IPersonPropsType {
+//     person:any;
+//     onChangedPerson?: any;
+//     onClickPerson?:any;
+// }
 
 
 
-// const preson: any = (props: IPersonPropsType) => {
-//     return <p>I'm {props.name} and my age is {props.age} </p>
-// };
+// // const preson: any = (props: IPersonPropsType) => {
+// //     return <p>I'm {props.name} and my age is {props.age} </p>
+// // };
 
-export default class Person extends React.Component<IPersonPropsType, any> {
-    constructor(props: IPersonPropsType) {
-        super(props);
-    }
+// export default class Person extends React.Component<IPersonPropsType, any> {
+//     constructor(props: IPersonPropsType) {
+//         super(props);
+//     }
 
-    public render() {
-        return (
-            <div className="Person">
-                <p onClick={this.props.click} >I'm {this.props.name} and my age is {this.props.age} </p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-        );
-    }
-}
+//     public render() {
+//         return (
+//             <div className="Person">
+//                 <p onClick={this.props.click} >I'm {this.props.name} and my age is {this.props.age} </p>
+//                 <p>{this.props.children}</p>
+//                 <input type="text" onChange={this.props.changed} value={this.props.name} />
+//             </div>
+//         );
+//     }
+// }
 
-// export default preson
+// // export default preson
